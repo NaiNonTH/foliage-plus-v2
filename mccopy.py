2717ad170c3e4b07e025274a0918b1b17baa7fcb
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2025-04-04 00:54:56 +0700

    add the main thing

--- mccopy.py
+++ mccopy.py
@@ -3,19 +3,20 @@ import shutil
 
 from argparse import ArgumentParser
 
-arg_parser = ArgumentParser()
-arg_parser.add_argument("-d", "--destination")
-
-dest_args = arg_parser.parse_args()
-
-if dest_args.destination is None:
-    print("Please provide destination path using -d or --destination.")
-    quit(1)
-
-DEST_DIR = dest_args.destination
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-shutil.rmtree(os.path.join(DEST_DIR, "Foliage+"))
-shutil.copytree(os.path.join(BASE_DIR, "pack"), os.path.join(DEST_DIR, "Foliage+"))
-
-print("copied!")
+if __name__ == "__main__":
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument("-d", "--destination")
+    
+    dest_args = arg_parser.parse_args()
+    
+    if dest_args.destination is None:
+        print("Please provide destination path using -d or --destination.")
+        quit(1)
+    
+    DEST_DIR = dest_args.destination
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    
+    shutil.rmtree(os.path.join(DEST_DIR, "Foliage+"))
+    shutil.copytree(os.path.join(BASE_DIR, "pack"), os.path.join(DEST_DIR, "Foliage+"))
+    
+    print("copied!")
